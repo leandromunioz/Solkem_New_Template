5c0137622773fa9d74389d30e06adb9ec703b881
Leandro Muñoz <47321348+leandromunioz@users.noreply.github.com>
2023-10-31 23:37:00 -0300

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+// Flecha de desplazamiento
+
 document.addEventListener("DOMContentLoaded", function () {
     const btnSubir = document.getElementById("btnSubir");
 
@@ -12,4 +14,31 @@ document.addEventListener("DOMContentLoaded", function () {
     btnSubir.onclick = function () {
         window.scrollTo({ top: 0, behavior: "smooth" });
     }
+});
+
+
+// Efecto hidden
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Obtener el footer
+    const footer = document.querySelector(".footer");
+
+    // Función para verificar si el footer está en el viewport
+    function isInViewport(element) {
+        const rect = element.getBoundingClientRect();
+        return (
+            rect.top <= (window.innerHeight || document.documentElement.clientHeight) &&
+            rect.left <= (window.innerWidth || document.documentElement.clientWidth)
+        );
+    }
+
+    // Escuchar el evento de scroll
+    window.addEventListener("scroll", function() {
+        // Si el footer está en el viewport y todavía tiene la clase 'footer-hidden'
+        if (isInViewport(footer) && footer.classList.contains("footer-hidden")) {
+            footer.classList.remove("footer-hidden");
+            footer.classList.add("footer-visible");
+        }
+    });
 });
