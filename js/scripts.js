135f775020696f33f9659139caf7df846eb20016
Leandro Muñoz <47321348+leandromunioz@users.noreply.github.com>
2023-11-03 20:51:07 -0300

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -1,44 +1,39 @@
+
+$(document).ready(function () {
+  $('#miTabla').DataTable();
+});
+
+
+
 // Flecha de desplazamiento
 
 document.addEventListener("DOMContentLoaded", function () {
-    const btnSubir = document.getElementById("btnSubir");
+  const btnSubir = document.getElementById("btnSubir");
 
-    window.onscroll = function () {
-        if (document.documentElement.scrollTop > 200 || document.body.scrollTop > 200) {
-            btnSubir.style.display = "block";
-        } else {
-            btnSubir.style.display = "none";
-        }
-    };
-
-    btnSubir.onclick = function () {
-        window.scrollTo({ top: 0, behavior: "smooth" });
+  window.onscroll = function () {
+    if (document.documentElement.scrollTop > 200 || document.body.scrollTop > 200) {
+      btnSubir.style.display = "block";
+    } else {
+      btnSubir.style.display = "none";
     }
+  };
+
+  btnSubir.onclick = function () {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  }
 });
 
 
-// Efecto hidden
+
+//NavBar flotante
 
 
-document.addEventListener("DOMContentLoaded", function() {
-    // Obtener el footer
-    const footer = document.querySelector(".footer");
+window.onscroll = function () {
+  var navbar = document.querySelector('.navbar');
+  if (window.pageYOffset > navbar.offsetTop) {
+    navbar.classList.add('fixed-top');
+  } else {
+    navbar.classList.remove('fixed-top');
+  }
+};
 
-    // Función para verificar si el footer está en el viewport
-    function isInViewport(element) {
-        const rect = element.getBoundingClientRect();
-        return (
-            rect.top <= (window.innerHeight || document.documentElement.clientHeight) &&
-            rect.left <= (window.innerWidth || document.documentElement.clientWidth)
-        );
-    }
-
-    // Escuchar el evento de scroll
-    window.addEventListener("scroll", function() {
-        // Si el footer está en el viewport y todavía tiene la clase 'footer-hidden'
-        if (isInViewport(footer) && footer.classList.contains("footer-hidden")) {
-            footer.classList.remove("footer-hidden");
-            footer.classList.add("footer-visible");
-        }
-    });
-});
